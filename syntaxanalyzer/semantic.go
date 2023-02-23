75ca65bb3e2a6dd18f422367fb8487c9b078b9da
Saf1u <a_safwa@live.concordia.ca>
2023-02-23 00:00:22 -0500

    node types

--- syntaxanalyzer/semantic.go
+++ syntaxanalyzer/semantic.go
@@ -2,8 +2,35 @@ package syntaxanalyzer
 
 type node interface {
 	makeSibling(node)
-	adoptChild(node)
-	nextSibling() node
-	addRightSibling(node)
-	addParent(node)
+	getRightSibling() node
+	getLeftMostSibling() node
+}
+
+type nodeImplementation struct {
+	parent          node
+	leftmostSibling node
+	leftmostChild   node
+	rightMost       node
+}
+
+type idNode struct {
+	identifier string
+	*nodeImplementation
+}
+
+func (i *nodeImplementation) makeSibling(y node) {
+	var current node = i
+	for current.getRightSibling() != nil {
+		current = current.getRightSibling()
+	}
+	y = y.getLeftMostSibling()
+	//could be itself
+
+}
+
+func (i *nodeImplementation) getRightSibling() node {
+	return i.rightMost
+}
+func (i *nodeImplementation) getLeftMostSibling() node {
+	return i.leftmostSibling
 }
