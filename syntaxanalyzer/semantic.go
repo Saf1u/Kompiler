d5d0f3732fb0d4620e3e37377b7eab89af44ce8e
Saf1u <a_safwa@live.concordia.ca>
2023-02-23 18:58:41 -0500

    adding nodes

--- syntaxanalyzer/semantic.go
+++ syntaxanalyzer/semantic.go
@@ -1,9 +1,14 @@
 package syntaxanalyzer
 
 type node interface {
-	makeSibling(node)
+	MakeSibling(node)
 	getRightSibling() node
 	getLeftMostSibling() node
+	getParent() node
+	setRightSibling(node)
+	setLeftSibling(node)
+	setParent(node)
+	AdoptChildren(node)
 }
 
 type nodeImplementation struct {
@@ -11,6 +16,7 @@ type nodeImplementation struct {
 	leftmostSibling node
 	leftmostChild   node
 	rightMost       node
+	lineNumber      int
 }
 
 type idNode struct {
@@ -18,19 +24,68 @@ type idNode struct {
 	*nodeImplementation
 }
 
-func (i *nodeImplementation) makeSibling(y node) {
+type typeNode struct {
+	typeName string
+	*nodeImplementation
+}
+
+type arraySizeNode struct {
+	*nodeImplementation
+}
+type epsilonNode struct {
+	*nodeImplementation
+}
+
+func (i *nodeImplementation) MakeSibling(y node) {
 	var current node = i
 	for current.getRightSibling() != nil {
 		current = current.getRightSibling()
 	}
 	y = y.getLeftMostSibling()
 	//could be itself
+	current.setRightSibling(y)
+	y.setLeftSibling(current.getLeftMostSibling())
+	y.setParent(current.getParent())
+	for y.getRightSibling() != nil {
+		y = y.getRightSibling()
+		y.setLeftSibling(current.getLeftMostSibling())
+		y.setParent(current.getParent())
+	}
+
+}
+func (i *nodeImplementation) AdoptChildren(y node) {
+	if i.leftmostChild != nil {
+		i.leftmostChild.MakeSibling(y)
+	} else {
+		left := y.getLeftMostSibling()
+		i.leftmostChild = left
+		for left != nil {
+			left.setParent(i)
+			left = left.getRightSibling()
+		}
+	}
 
 }
 
 func (i *nodeImplementation) getRightSibling() node {
 	return i.rightMost
 }
+func (i *nodeImplementation) getParent() node {
+	return i.parent
+}
 func (i *nodeImplementation) getLeftMostSibling() node {
+	if i.leftmostSibling == nil {
+		return i
+	}
 	return i.leftmostSibling
+}
+
+func (i *nodeImplementation) setRightSibling(y node) {
+	i.rightMost = y
+}
+func (i *nodeImplementation) setLeftSibling(y node) {
+	i.leftmostSibling = y
+}
+func (i *nodeImplementation) setParent(y node) {
+	i.parent = y
 }
