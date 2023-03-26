7b16c35fed2e334bf57f7d53bb2ac18a7254b5b6
Saf1u <a_safwa@live.concordia.ca>
2023-03-26 16:58:04 -0400

    safe file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go build  .
-./compiler -file files/semantictests/testc.src 
-cat  files/semantictests/testc.src-data.m  files/semantictests/testc.src-code.m>moonBin/final.m
-moon moonBin/final.m moonBin/lib.m
+./compiler -file files/semantictests/wemove.src 
+cat  files/semantictests/wemove.src-data.m  files/semantictests/wemove.src-code.m>moonBin/wemovefinal.m
+moon moonBin/wemovefinal.m moonBin/lib.m
