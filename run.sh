981625ffdeb36ab6d4d3e30c8925df51872a4c58
Saf1u <a_safwa@live.concordia.ca>
2023-03-27 01:14:38 -0400

    run takes in file arg

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go build  .
-./compiler -file files/semantictests/wemove.src 
-cat  files/semantictests/wemove.src-data.m  files/semantictests/wemove.src-code.m>moonBin/wemovefinal.m
-moon moonBin/wemovefinal.m moonBin/lib.m
+./compiler -file files/semantictests/$1
+cat  files/semantictests/$1-data.m  files/semantictests/$1-code.m>moonBin/$1.m
+moon moonBin/$1.m moonBin/lib.m
