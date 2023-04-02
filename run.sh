ba43074c82cbe0fa7261de3df426ce1283bc9a8e
Saf1u <a_safwa@live.concordia.ca>
2023-04-02 04:07:17 -0400

    made run from any directory

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
  go build  .
- ./compiler -file files/semantictests/$1
- cat  files/semantictests/$1-data.m  files/semantictests/$1-code.m>moonBin/$1.m
- moon moonBin/$1.m moonBin/lib.m
+ ./compiler -file $1
+ fname=$(basename  $1)
+ cat  $1-data.m  $1-code.m>moonBin/$fname.m
+ moon moonBin/$fname.m moonBin/lib.m
