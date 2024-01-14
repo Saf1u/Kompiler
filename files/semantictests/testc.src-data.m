49c3201b72ff54843f32c6bbcd46edcfa19a0038
Saf1u <a_safwa@live.concordia.ca>
2024-01-14 13:37:13 -0500

    ignore

--- files/semantictests/testc.src-data.m
+++
@@ -1,4 +0,0 @@
-buffer               res     200
-stackbase            res     2048
-newline              db      ,13,10,0
-align
--- files/semantictests/variables.src-data.m
+++
@@ -1,5 +0,0 @@
-org 0
-buffer               res     200
-stackbase            res     4096
-newline              db      ,13,10,0
-align
